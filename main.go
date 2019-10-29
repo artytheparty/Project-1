fb0c95a476e2493ea54253214e904d4f10e7bc6e
artytheparty <artkhv15@protonmail.com>
2019-10-29 03:00:47 -0500

    Created super struct to hold all of encoded data

--- main.go
+++ main.go
@@ -9,11 +9,21 @@ import (
 	"os"
 
 	"github.com/artytheparty/project-1/cpumem"
+	"github.com/artytheparty/project-1/cpuusage"
+	"github.com/artytheparty/project-1/lscpu"
 	"github.com/artytheparty/project-1/sysinfo"
 )
 
 const systemInfoLoc string = "$HOME/systemvar.txt"
 const systemProcInfoLoc string = "$HOME/processtable.txt"
+
+//SuperStruct holds all of the data
+type SuperStruct struct {
+	Sysinfo  sysinfo.SysInfo   `json:"SYSINFO"`
+	Lscpu    lscpu.LSCPU       `json:"LSCPU"`
+	CPUUsage cpuusage.CPUUsage `json:"CPUUSAGE"`
+	Cpumem   cpumem.CPUTOP     `json:"CPUMEM"`
+}
 
 func main() {
 	//creates a logging file when error occurs
@@ -25,28 +35,28 @@ func main() {
 	log.SetOutput(file)
 	//
 	sysinfo.CreateSystemInfoFile2()
-	fmt.Println(sysinfo.ReadSysInfo())
+	//fmt.Println(sysinfo.ReadSysInfo())
 	//
-	sysinfo.CreateLSCPUFILE()
-	fmt.Println(sysinfo.ReadLSCPUCommand())
+	lscpu.CreateLSCPUFILE()
+	//fmt.Println(lscpu.ReadLSCPUCommand())
+
+	//takes in the cpu usage
+	cpuusage.CreateCPUUsage()
+	//fmt.Println(cpuusage.GetCPUUsage())
 	//
 	cpumem.CreateTopSnapshot()
-	fmt.Print(cpumem.GetTopSnapshot().Processes)
-	//takes in the cpu usage
-	//cpumem.CreateCPUUsage()
-	//fmt.Println(cpumem.GetCPUUsage())
-
-	// http.Handle("/", http.FileServer(http.Dir("client")))
-	// http.HandleFunc("/sse/serveUpdateddata", serveUpdateddata)
-	// http.ListenAndServe(":8080", nil)
+	//fmt.Print(cpumem.GetTopSnapshot())
+	http.Handle("/", http.FileServer(http.Dir("client")))
+	http.HandleFunc("/sse/serveUpdateddata", serveUpdateddata)
+	http.ListenAndServe(":8080", nil)
 }
 
 func serveUpdateddata(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
-	cpumem.CreateCPUUsage()
-	holder := cpumem.GetCPUUsage()
+	cpuusage.CreateCPUUsage()
+	holder := cpuusage.GetCPUUsage()
 	var buf bytes.Buffer
 	enc := json.NewEncoder(&buf)
 	enc.Encode(holder)
